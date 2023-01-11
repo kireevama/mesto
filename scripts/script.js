f420f51df7dd2427e806e9279c6a968339ce4f4b
Marina Kireeva <kireevama43@yandex.ru>
2023-01-11 17:54:32 +0300

    fix: исправить сетку и верстку блока places card

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,41 @@
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__container');
-const nameInput = document.querySelector('.popup__name');
-const descriptionInput = document.querySelector('.popup__description');
-const peprofileEditButton = document.querySelector('.profile__edit-button');
+const form__item_el_name = document.querySelector('.popup__name');
+const form__item_el_description = document.querySelector('.popup__description');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
+const popupEditForm = document.querySelector('.popup__edit-form');
 
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-peprofileEditButton.addEventListener('click', openPopup);
 function openPopup() {
   popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;
+  form__item_el_name.value = profileName.textContent;
+  form__item_el_description.value = profileDescription.textContent;
 }
+profileEditButton.addEventListener('click', openPopup);
 
-popupCloseButton.addEventListener('click', closePopup);
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
+popupCloseButton.addEventListener('click', closePopup);
 
-popup.addEventListener('click', function(event) {
-  if(!event.defaultPrevented) {
-    closePopup();
-  }
-}
-)
-formElement.addEventListener('click', function(event) {
-  event.preventDefault();
-}
-)
+// popup.addEventListener('click', function(event) {
+//   if(!event.defaultPrevented) {
+//     closePopup();
+//   }
+// }
+// )
+// popupEditForm.addEventListener('click', function(event) {
+//   event.preventDefault();
+// }
+// )
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-  popup.classList.remove('popup_opened');
-};
-
-const saveButton = document.querySelector('.popup__save-button');
-saveButton.addEventListener('click', handleFormSubmit);
+  profileName.textContent = form__item_el_name.value;
+  profileDescription.textContent = form__item_el_description.value;
+  closePopup();
+}
+popupEditForm.addEventListener('submit', handleFormSubmit);
